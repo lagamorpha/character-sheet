65b79b732b68c16cb96268a39e8a1226d2e49ecc
Michael Chandler <macjr93@yahoo.com>
2022-08-24 20:46:48 -0700

    make first trial multi-die roller

--- app.js
+++ app.js
@@ -7,8 +7,16 @@ let die6 = Math.floor(rand * 6 + 1);
 // console.log(die6);
 
 // advanced variable die function
-const die = function (sides) {
+const die = (sides) => {
     return Math.floor(rand * sides + 1);
 }
-console.log(die(10));
+
+// multiple die function
+
+// attempt 1 (works, but just copys first die)
+// let total = 0;
+// for (i = 1; i <= 3; i++) {
+//     total += die6;
+// }
+// console.log(total);
 
